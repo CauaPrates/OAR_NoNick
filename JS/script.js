f80f92f2508191091b45e20d2768d929c863fca5
SamuelSoaresSilva <samuel.soares.cunha64@gmail.com>
2023-08-19 02:30:42 -0300

    JS  Added / <main> done

---
+++ JS/script.js
@@ -0,0 +1,14 @@
+const produto = {
+
+    ProdName: "Camisa lisa",
+    ProdDescription: null,
+    ProdId: null,
+    ProdColor: null,
+    ProdSize: null,
+    ProdImage: null,
+    ProdPrice: "2,20"
+
+};
+
+document.getElementById("ProdName").textContent = produto.ProdName;
+document.getElementById("ProdPrice").textContent = produto.ProdPrice;
